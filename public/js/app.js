90c43a85ba8b5daa273005c3545a28fb798c3189
IntInternational <103089998+IntInternational@users.noreply.github.com>
2024-02-06 17:58:14 -0500

    First Commit

---
+++ public/js/app.js
@@ -0,0 +1,62 @@
+class ParentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonColor1: 'blue',
+            buttonColor2: 'blue',
+            buttonName1: 'GO CHIEFS',
+            buttonName2: 'GO 49ERS',
+        };
+    }
+
+    changeButtonColor1 = () => {
+        const colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        this.setState({ buttonColor1: randomColor });
+    }
+    changeButtonColor2 = () => {
+        const colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        this.setState({ buttonColor2: randomColor });
+    }
+
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                <ChildComponent
+                    buttonName= {this.state.buttonName1}
+                    buttonColor={this.state.buttonColor1}
+                    changeButtonColor={this.changeButtonColor1}
+                />
+                <ChildComponent
+                    buttonName= {this.state.buttonName2}
+                    buttonColor={this.state.buttonColor2}
+                    changeButtonColor={this.changeButtonColor2}
+                />
+            </div>
+        );
+    }
+}
+
+class ChildComponent extends React.Component {
+    render() {
+        return (
+            <div className='ui centered card'>
+                <div className='field'>
+                    <label>{this.props.buttonName}</label>
+                </div>
+                <button
+                    className={`ui basic ${this.props.buttonColor} button`}
+                    onClick={this.props.changeButtonColor}
+                >
+                    Submit!
+                </button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ParentComponent />,
+    document.getElementById("content")
+);
